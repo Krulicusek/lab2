3ca810229d6b7cba29bd8284c037729487219f05
kubii <kubawegrzynowski@gmail.com>
2022-10-18 12:49:10 +0200

    Add project files.

---
+++ lab2/CarUtility.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    public class CarUtility
+    {
+        private BackgroundWorker backgroundWorker;
+        public CarUtility()
+        {
+            InitializeBackgroundWorker();
+        }
+        private void InitializeBackgroundWorker()
+        {
+            backgroundWorker = new BackgroundWorker();
+            backgroundWorker.WorkerSupportsCancellation = true;
+            backgroundWorker.WorkerReportsProgress = true;
+            backgroundWorker.DoWork += new DoWorkEventHandler(BackgroundWorker_DoWork);
+        }
+
+        public void ChangeTire()
+        {
+
+        }
+
+        public void StopSign(bool shouldWork)
+        {
+            if (shouldWork)
+            {
+                backgroundWorker.RunWorkerAsync();
+            }
+            else
+            {
+                backgroundWorker.CancelAsync();
+            }
+        }
+
+        private void BackgroundWorker_DoWork(object? sender, DoWorkEventArgs e)
+        {
+            if (sender == null)
+            {
+                return;
+            }
+            BackgroundWorker bw = sender as BackgroundWorker;
+            if (bw.CancellationPending)
+            {
+                e.Cancel = true;
+            }
+            e.Result = LogState(bw, e);
+        }
+        private async Task LogState(BackgroundWorker bw, DoWorkEventArgs e)
+        {
+            while (true)
+            {
+                Thread.Sleep(100);
+                Console.WriteLine("stop sign is active");
+                //Check if there is a request to cancel the process
+                if (backgroundWorker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ lab2/Program.cs
@@ -0,0 +1,8 @@
+﻿using lab2;
+
+CarUtility carUtility = new CarUtility();
+carUtility.StopSign(true);
+for (int i = 0; i < 10; i++)
+{
+
+}
